48ed668c4c1a4613ad97e9b125cdecc55ceb665f
Aman Kumar <onlyoneaman>
2023-12-27 19:13:13 +0530

    Home Action

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
 
   return (
     <header className="header flex justify-between">
-      <div className="header__logo">Movies Recommender</div>
+      <a className="" href={"/"}>Movies Recommender</a>
       {/*<div className="header__menu">Menu</div>*/}
     </header>
   );
